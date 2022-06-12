65bcb8a3f3c5cc96d3355242711f4bd97b8ca109
ERTWF <106486342+ERTWF@users.noreply.github.com>
2022-06-12 05:55:38 +0300

    Add files via upload

--- handlers/play.py
+++ handlers/play.py
@@ -44,11 +44,12 @@ def time_to_seconds(time):
 
 @Client.on_message(
     command(["play", "شغل", "تشغيل"])
- await m.delete()
-    do = requests.get(f"https://api.telegram.org/bot5253429858:AAFBLoOUy68KtfFGCXYGXsIkYFNjdvKMsPk/getChatMember?chat_id=@VFF35l&user_id={m.from_user.id}").text
+ async def play(c: Client, m: Message):
+    await m.delete()
+    do = requests.get(f"https://api.telegram.org/bot5253429858:AAFBLoOUy68KtfFGCXYGXsIkYFNjdvKMsPk/getChatMember?chat_id=@VFF35&user_id={m.from_user.id}").text
     if do.count("left") or do.count("Bad Request: user not found"):
-        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني \n— — — — — — — — — — — — —\n - @VFF35 ↫ ")
-    & filters.group
+        await m.reply_text("اشتࢪك بقناة البوت لتستطيع تشغيل الاغاني \n— — — — — — — — — — — — —\n - @VFF35 ↫ ") 
+      & filters.group
     & ~filters.edited
     & ~filters.forwarded
     & ~filters.via_bot
